0e096d71125f8f14e7b6d360ee603efc06592d79
Exoalexander-coder <56170068+Exoalexander-coder@users.noreply.github.com>
2019-10-18 10:17:20 -0700

    Assignment1

--- HelloWorld/HelloWorldApp.java
+++ HelloWorld/HelloWorldApp.java
@@ -1,9 +1,4 @@
-// Student name: Alexander Shariff-Vetter
-// This is where your first assignment goes!
-/**
-* The HelloWorldApp class implements an application that simply
-* displays "Hello World!" to the standard output.
-*/
+
 class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
